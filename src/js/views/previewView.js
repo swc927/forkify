e2dade58e1b5bd1280ea8347bcb4ff3655132637
swc927 <siuwengchun@gmail.com>
2025-09-08 12:57:43 +0800

    Initial commit

---
+++ src/js/views/previewView.js
@@ -0,0 +1,38 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class PreviewView extends View {
+  _parentElement = '';
+
+  _generateMarkup() {
+    const id = window.location.hash.slice(1);
+    const result = this._data;
+
+    return `
+        <li class="preview">
+            <a class="preview__link ${
+              this._data.id === id ? 'preview__link--active' : ''
+            }" href="#${result.id}">
+            <figure class="preview__fig">
+                <img src="${result.image}" alt="${result.title}" />
+            </figure>
+            <div class="preview__data">
+                <h4 class="preview__title">
+                ${result.title}
+                </h4>
+                <p class="preview__publisher">${result.publisher}</p>
+                <div class="preview__user-generated ${
+                  this._data.key ? '' : 'hidden'
+                }">
+                    <svg>
+                      <use href="${icons}#icon-user"></use>
+                    </svg>
+                  </div>
+                </div>
+            </a>
+        </li>
+        `;
+  }
+}
+
+export default new PreviewView();
